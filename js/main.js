0ef06eaf7c2977ae167d86acb2f2d9d97e09f2a1
Jun-Wei Zeng <zengjunwei@stu.vma.edu.cn>
2026-03-06 19:08:58 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-
+fetch('data/events.json')
+    .then(response => response.json())
+    .then(events => {
+        const container = document.getElementById('events-container');
+        events.forEach(event => {
+            container.innerHTML += `
+                <div class="event-item">
+                    <div class="event-content">
+                        <div class="event-date">${event.date}</div>
+                        <h3>${event.title}</h3>
+                        <p>${event.description}</p>
+                        <a href="${event.link}" class="cta-button">Register Now</a>
+                    </div>
+                </div>
+            `;
+        });
+    });
